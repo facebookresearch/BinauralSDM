1eefde79c231720a672907dad850e50b526ca533
Hannes Helmholz <hannes.helmholz@chalmers.se>
2021-09-15 20:23:42 +0200

    Update `Read_HRTF.m` to give instructions if SOFA API is unavailable

--- Src/Read_HRTF.m
+++ Src/Read_HRTF.m
@@ -5,6 +5,28 @@ function HRTF_data = Read_HRTF(BRIR_data)
 if strcmp(BRIR_data.HRTF_Type, 'FRL_HRTF')
     HRTF_data = load(BRIR_data.HRTF_Path);
 elseif strcmp(BRIR_data.HRTF_Type, 'SOFA')
+    try
+        SOFAstart;
+    catch ME
+        if strcmp(ME.identifier, 'MATLAB:UndefinedFunction')
+            warning('MATLAB:UndefinedFunction', ['SOFA library not found in MATLAB path.\n'...
+                'Please download the API from https://github.com/sofacoustics/API_MO\n',...
+                'and link to the folder in the opened file dialog.']);
+
+            % show file dialog to link to API folder
+            SOFA_dir = uigetdir('', 'Select SOFA API root folder');
+
+            % add directory to path
+            fprintf('Adding "%s" to MATLAB path.\n', SOFA_dir); 
+            addpath(genpath(SOFA_dir));
+
+            % now run SOFAstart (hopefully)
+            SOFAstart;
+        else
+            rethrow(ME);
+        end
+    end 
+    
     HRTF_data = SOFAload(BRIR_data.HRTF_Path);
 else
     error('HRTF format not recognized - Valid options: FRL_HRTF, SOFA.')
