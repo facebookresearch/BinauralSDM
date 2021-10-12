eb79cd30890ec75c9c8bd2e4057fa07614c3b16f
Hannes Helmholz <hannes.helmholz@chalmers.se>
2021-10-13 00:07:47 +0200

    Update `SaveRenderingStructs.m` similarly to `SaveBRIR.m`

--- Src/SaveRenderingStructs.m
+++ Src/SaveRenderingStructs.m
@@ -9,15 +9,17 @@ function SaveRenderingStructs(SRIR_data, BRIR_data)
 %   - BRIR_data: Struct containing BRIR information (see create_BRIR_data)
 %
 %   Author: Sebastià V. Amengual
-%   Last modified: 7/11/19
+%   Last modified: 10/12/21
 
-Save_Path = [BRIR_data.DestinationPath  regexprep(BRIR_data.HRTF_Subject,' ','_'), filesep SRIR_data.Room '_' SRIR_data.SourcePos '_' SRIR_data.ReceiverPos];
+Save_Path = fullfile(BRIR_data.DestinationPath, ...
+    strrep(BRIR_data.HRTF_Subject, ' ', '_'), ...
+    sprintf('%s_%s_%s', SRIR_data.Room, SRIR_data.SourcePos, SRIR_data.ReceiverPos), ...
+    BRIR_data.RenderingCondition);
 
-if ~exist([Save_Path filesep BRIR_data.RenderingCondition filesep], 'dir')
-    mkdir([Save_Path filesep BRIR_data.RenderingCondition filesep]);
-end
+% create output directory (ignore if it already exists)
+[~, ~] = mkdir(Save_Path);
 
-save([Save_Path filesep BRIR_data.RenderingCondition filesep 'SRIR_data.mat'],'SRIR_data');
-save([Save_Path filesep BRIR_data.RenderingCondition filesep 'BRIR_data.mat'],'BRIR_data');
+save(fullfile(Save_Path, 'SRIR_data.mat'), 'SRIR_data');
+save(fullfile(Save_Path, 'BRIR_data.mat'), 'BRIR_data');
 
 end
