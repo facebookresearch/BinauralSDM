f35912221a31c435d25861675d6574dea6237d2f
Hannes Helmholz <hannes.helmholz@chalmers.se>
2021-11-12 12:31:37 +0100

    Add check to skip `Smooth_DOA.m` if number of samples smaller than 2

--- Src/Smooth_DOA.m
+++ Src/Smooth_DOA.m
@@ -9,6 +9,8 @@ function SRIR_data = Smooth_DOA(SRIR_data)
 %
 % To-Do: Include DBSCAN clustering
 
+if SRIR_data.DOASmooth < 2; return; end
+
 fprintf('Smoothing DOA using a window of %d samples.\n', SRIR_data.DOASmooth);
 SRIR_data.DOA(:,1) = movmean(SRIR_data.DOA(:,1),SRIR_data.DOASmooth);
 SRIR_data.DOA(:,2) = movmean(SRIR_data.DOA(:,2),SRIR_data.DOASmooth);
